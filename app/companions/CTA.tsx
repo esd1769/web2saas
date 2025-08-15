81fc4daa92decebba13ef08998c6effa43898c50
esd1769 <126pubgwarrior@gmail.com>
2025-08-15 14:41:22 -0700

    Create CTA.tsx

---
+++ app/companions/CTA.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Cta = () => {
+    return (
+        <section className="cta-section">
+            <div className="cta-badge">Start learning your lesson</div>
+
+        </section>
+
+    )
+}
+export default Cta
