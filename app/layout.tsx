fc2bc0955771d615c70c08d4ccfe92dc0eb604ff
esd1769 <126pubgwarrior@gmail.com>
2025-08-16 08:41:33 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${bricolage.variable} antialiased`}>
-      <Navbar />
+      <Navbar  />
       {children}
       </body>
     </html>
