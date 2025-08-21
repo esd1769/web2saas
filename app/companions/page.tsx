4bb145d6cd6c65b70aed18a11cfcd9af6eb37963
esd1769 <126pubgwarrior@gmail.com>
2025-08-21 10:50:36 -0700

    Update page.tsx

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,8 +1,40 @@
+import {getAllCompanions} from "@/lib/actions/companion.actions";
+import CompanionCard from "@/app/companions/CompanionCard";
+import {getSubjectColor} from "@/lib/utils";
+import SearchInput from "@/components/SearchInput";
+import SubjectFilter from "@/components/SubjectFilter";
 
 
-const CompanionsLibrary = () => {
+const CompanionsLibrary = async({searchParams}:SearchParams) => {
+   const filter=await searchParams;
+   const subject=filter.subject?filter.subject:"";
+   const topic=filter.topic?filter.topic:"";
+   const companions=await getAllCompanions({subject,topic});
+   //console.log('param=',companions);
+    //default flex  alignment is justify-start..horizental
     return (
-        <div>Companions</div>
+        <main className="flex justify-center gap-4 max-sm:flex-col">
+            <section>
+                <h1>Companion Library</h1>
+                <div className=" flex gap-4">
+                    <SearchInput/>
+                    <SubjectFilter/>
+                </div>
+            </section>
+
+            <section className="companions-grid">
+                {companions.map((companion) => (
+                    <CompanionCard key={companion.id}{...companion}
+                        color={getSubjectColor(companion.subject)}
+
+                    />
+
+                ))}
+
+            </section>
+
+
+            </main>
     )
 }
 export default CompanionsLibrary
