2e7a26c9489e5998a84e0688ac86591a14808ae6
esd1769 <126pubgwarrior@gmail.com>
2025-08-26 10:24:47 -0700

    Update CompanionComponent.tsx

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -1,7 +1,12 @@
 'use client'
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import {cn, getSubjectColor} from "@/lib/utils";
 import {vapi} from "@/lib/vapi.sdk";
+import Image from "next/image";
+import Lottie,{LottieRefCurrentProps} from "lottie-react";
+import soundwaves from '@/constants/soundwaves.json'
+
+
 enum CallStatus {
     INACTIVE = 'INACTIVE',
     CONNECTING = 'CONNECTING',
@@ -15,12 +20,21 @@ const CompanionComponent =
         const [callStatus, setCallStatus] =
             useState<CallStatus>(CallStatus.INACTIVE);
         const [isSpeaking, setIsSpeaking] = useState(false);
+        const [isMuted, setIsMuted] = useState(false);
+        const lottieRef = useRef<LottieRefCurrentProps>(null);
+
 
         useEffect(() => {
-            const onCallStart = ()
-                => setCallStatus(CallStatus.ACTIVE);
-            const onCallEnd = ()
-                =>setCallStatus(CallStatus.FINISHED);
+            if (lottieRef){
+                if (isSpeaking) lottieRef.current ?.play()
+                else lottieRef.current?.stop()
+            }
+
+        }, [isSpeaking,lottieRef]);
+
+        useEffect(() => {
+            const onCallStart = () => setCallStatus(CallStatus.ACTIVE);
+            const onCallEnd = () =>setCallStatus(CallStatus.FINISHED);
             const onMessage = () => {}
             const onSpeechStart = () => {setIsSpeaking(true);};
             const onSpeechEnd = () => setIsSpeaking(false);
@@ -43,6 +57,28 @@ const CompanionComponent =
             }
 
         },[])
+        const toggleMicrophone=()=>{
+            const isMuted=vapi.isMuted();
+            vapi.setMuted(!isMuted);
+            setIsMuted(!isMuted);
+        };
+        const handleCall=async()=>{
+            setCallStatus(CallStatus.CONNECTING);
+            const assistantOverrides={
+                variableValues:{
+                    subject,topic,style
+
+                },
+                clientMessages:['transcript'],
+                serverMessages:[],
+
+            }
+            vapi.start()
+
+        }
+        const handleDisconnectCall=()=>{
+
+        }
 
     return (
 
@@ -50,21 +86,87 @@ const CompanionComponent =
 
     <section className='flex flex-col h-[70vh]'>
             <section className='flex gap-8 max-sm:flex-col '>
-
-
                 <div className='companion-section'>
-
                     <div className='companion-avatar' style={{backgroundColor:
-                            getSubjectColor(subject)}} >
-                        <div className={cn(...inputs:
-                            'absolute transition-opacity duration-1000')}>
+                            getSubjectColor(subject)} }>
+                        <Image src={`/icons/${subject}.svg`} alt={subject}
+                               width={150} height={150} className='max-sm:w-fit'/>
 
 
+
+                        <div
+                            className={
+                            cn(
+                                'absolute transition-opacity duration-1000',
+                                callStatus === CallStatus.FINISHED ||
+                                callStatus === CallStatus.INACTIVE ? 'opacity-100' : 'opacity-0',
+                                callStatus === CallStatus.CONNECTING && 'opacity-100 animate-pulse',
+                                )
+                        }>
+                            {/*<Image src='/icons/headset.svg' alt='headset'*/}
+                            {/*       width={100} height={100}*/}
+                            {/*       className='companion-headset'/>*/}
                         </div>
+                        <div
+                            className={
+                                cn(
+                                    'absolute transition-opacity duration-1000',
+                                    callStatus === CallStatus.ACTIVE ? 'opacity-100' : 'opacity-0')}>
 
+                            <Lottie
+                                lottieRef={lottieRef}
+                                animationData={soundwaves}
+                                autoPlay={false}
+                                className="companion-lottie"
+                            />
+                        </div>
                     </div>
+                <p className='font-bold text-2xl'>{name}</p>
+            </div>
+                <div className='user-section'>
+                    <div className='user-avatar'>
+                        <Image src={userImage}
+                               alt={userImage} width={130} height={130}
+                               className='rounded-lg'/>
+                        <p className='font-bold text-2xl'>{userName}</p>
+                    </div>
+                    <button className='btn-mic' onClick={toggleMicrophone}>
+
+                            <Image src={isMuted ? '/icons/mic-off.svg':'/icons/mic-on.svg'}alt='mic'
+                                   width={36} height={36}/>
+                        <p className='max-sm:hidden'>
+                            {isMuted ? 'Turn on microphone':' turn off micprophone'}
+
+                        </p>
+                    </button>
+                    <button className={cn(
+                        'rounded-lg py-2 cursor-pointer transition-colors w-full text-white',
+                        callStatus === CallStatus.ACTIVE ? 'bg-red-700' : 'bg-primary',
+                        callStatus === CallStatus.CONNECTING && 'animate-pulse'
+                    )}
+                            onClick={callStatus === CallStatus.ACTIVE ?
+                                handleDisconnectCall : handleCall}>
+
+                        {
+                            callStatus === CallStatus.ACTIVE
+                                ? 'End Session'
+                        : callStatus === CallStatus.CONNECTING
+                            ? 'Connecting...'
+                            : 'Start Session'
+                    }
+                    </button>
                 </div>
+
+
             </section>
+        <section className='transcript'>
+            <div className='transcript-message no-scrollbar'>
+                Message Transcript will appear here...
+            </div>
+            <div className='transcript-fade'/>
+
+        </section>
+
         </section>
     )
 }
